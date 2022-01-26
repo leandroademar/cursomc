14457750e64037615063bad0e059383e79f50b2a
Leandro Ademar <leandro.ademars@gmail.com>
2022-01-26 11:52:46 -0300

    Aula 18

---
+++ src/main/java/com/datitecnologia/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.datitecnologia.cursomc.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.datitecnologia.cursomc.domain.Categoria;
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
