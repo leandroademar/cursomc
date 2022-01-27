da95f2ce11324a46f25e8a2feab6d4be72fe4c33
Leandro Ademar <leandro.ademars@gmail.com>
2022-01-26 23:08:32 -0300

    Endpoint Clientes

--- src/main/java/com/datitecnologia/cursomc/domain/Cidade.java
+++ src/main/java/com/datitecnologia/cursomc/domain/Cidade.java
@@ -1,7 +1,6 @@
 package com.datitecnologia.cursomc.domain;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,10 +12,8 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Cidade implements Serializable {
-
-
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
@@ -25,11 +22,10 @@ public class Cidade implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="estado_id")
 	private Estado estado;
-
-	public Cidade() {
-		
-	}
 	
+	public Cidade() {
+	}
+
 	public Cidade(Integer id, String nome, Estado estado) {
 		super();
 		this.id = id;
@@ -63,7 +59,10 @@ public class Cidade implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -75,8 +74,14 @@ public class Cidade implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Cidade other = (Cidade) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
+	
 	
 	
 }
--- src/main/java/com/datitecnologia/cursomc/domain/Estado.java
+++ src/main/java/com/datitecnologia/cursomc/domain/Estado.java
@@ -3,7 +3,6 @@ package com.datitecnologia.cursomc.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,62 +10,61 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Estado implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
-	@OneToMany(mappedBy = "estado")
+	@JsonIgnore
+	@OneToMany(mappedBy="estado")
 	private List<Cidade> cidades = new ArrayList<>();
-
 	
+	public Estado() {
+	}
+
 	public Estado(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 
-
 	public Integer getId() {
 		return id;
 	}
-
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-
 	public String getNome() {
 		return nome;
 	}
-
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
-
 	public List<Cidade> getCidades() {
 		return cidades;
 	}
-
 
 	public void setCidades(List<Cidade> cidades) {
 		this.cidades = cidades;
 	}
 
-
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -77,11 +75,12 @@ public class Estado implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Estado other = (Estado) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	
-	
-	
-
 	
 }
